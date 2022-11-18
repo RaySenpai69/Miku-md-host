8f8833f7324fd4397a5da2710b328625b8f53166
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2022-11-18 20:18:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
-sudo pacman -Syu
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ../
-sudo pacman -S ffmpeg 
-sudo pacman -S imagemagick
-sudo pacman -S npm
-sudo pacman -S nodejs
-sudo pacman -S libwebp
-yay -S yarn-completion-git
-git clone https://github.com/FantoX001/Miku-MD.git
+sudo apt update
+sudo apt upgrade
+sudo apt install bash
+sudo apt-get install libwebp-dev
+sudo apt install git
+sudo apt install nodejs -y
+sudo apt install ffmpeg -y
+sudo apt install npm
+sudo apt install imagemagick
+git clone https://github.com/FantoX001/Miku-MD
+npm install --global yarn
+yarn add sharp
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+yarn add sharp
 cd Miku-MD
-yarn
-yarn start
+npm i
+npm start
